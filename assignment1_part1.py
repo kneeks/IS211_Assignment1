6c005ae6a23fd5aca889741d0198264c8ec9260e
Niko Vaccaro <mr.nikovaccaro@gmail.com>
2016-02-06 15:41:27 -0500

    part one complete

---
+++ assignment1_part1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Week 1 task assignment
+"""
+
+class ListDivideException(Exception):
+        """Raise Exception"""
+
+
+def listDivide(numbers, divide=2):
+    count = 0
+    for x in numbers:
+        if x % divide == 0:
+            count += 1
+    return count
+
+
+def testListDivide():
+    a = listDivide([1,2,3,4,5])
+    b = listDivide([2,4,6,8,10])
+    c = listDivide([30,54,63,98,100], divide=10)
+    d = listDivide([])
+    e = listDivide([1,2,3,4,5], 1)
